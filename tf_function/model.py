26f58d0fa766e8ed6d47dc21604d93fef593ad49
Kam Kasravi <kam.d.kasravi@intel.com>
2020-03-02 15:31:22 +0000

    added tf_function

---
+++ tf_function/model.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+
+import tensorflow as tf
+from tensorflow import keras
+
+
+# The function to be traced.
+@tf.function
+def my_func(x, y):
+  # A simple hand-rolled layer.
+  return tf.nn.relu(tf.matmul(x, y))
+
+# Set up logging.
+stamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+logdir = 'logs/func/%s' % stamp
+writer = tf.summary.create_file_writer(logdir)
+
+# Sample data for your function.
+x = tf.random.uniform((3, 3))
+y = tf.random.uniform((3, 3))
+
+# Bracket the function call with
+# tf.summary.trace_on() and tf.summary.trace_export().
+tf.summary.trace_on(graph=True, profiler=True)
+# Call only one tf.function when tracing.
+z = my_func(x, y)
+with writer.as_default():
+  tf.summary.trace_export(
+      name="my_func_trace",
+      step=0,
+      profiler_outdir=logdir)
