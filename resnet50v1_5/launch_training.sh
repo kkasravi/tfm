4fb3e36404b5f498da2c4b6de10ce66d9b37685a
Kam D Kasravi <kam.d.kasravi@intel.com>
2020-04-10 19:54:28 +0000

    add training-mpi

--- resnet50v1_5/launch_training.sh
+++ resnet50v1_5/launch_training.sh
@@ -5,6 +5,8 @@ BENCHMARKS=${BENCHMARKS}
 PWD=${PWD-$(pwd)}
 KMP_AFFINITY=${KMP_AFFINITY-'granularity=fine,verbose,compact,1,0'}
 NUM_CORES=${NUM_CORES-20}
+MPI_NUM_PROCESSES=${MPI_NUM_PROCESSES-''}
+PRECISION=${PRECISION-fp32}
 export MKL_VERBOSE=1
 export MKLDNN_VERBOSE=1
 export DNNL_VERBOSE=1
@@ -16,8 +18,9 @@ pyenv which python
 rm -rf checkpoints logs && mkdir checkpoints logs && \
 $PYTHON_EXE ${BENCHMARKS}/launch_benchmark.py \
          --model-name=resnet50v1_5 \
-         --precision=fp32 \
+         --precision=$PRECISION \
          --mode=training \
          --framework=tensorflow \
          --checkpoint=$PWD/checkpoints \
+         $MPI_NUM_PROCESSES \
          --data-location=/tf_dataset/dataset/TF_Imagenet_FullData --steps=$STEPS 2>&1 | tee $PWD/logs/output.log
